d7c0a86169c20fc47a09cbab888b4d097bae3df9
kalinasasha30 <112653857+kalinasasha30@users.noreply.github.com>
2022-10-21 19:04:02 +0300

    Add files via upload

---
+++ Hometask5/main.js
@@ -0,0 +1,52 @@
+// №2
+
+// typeof undefined // "undefined"
+
+// typeof 0 // number 
+
+// typeof 1n // bigint
+
+// typeof true // boolean
+
+// typeof "foo" // string
+
+// typeof Symbol() // symbol
+
+// typeof {} // object
+
+// typeof null // object
+
+// typeof function(){} // function
+
+// №3
+
+// let a = prompt('Введите сюда любое число')
+//     if (a == 10) {
+//         alert('Верно')
+//     }   
+//     else {
+//         alert('Неверно')
+//     }
+
+// №4
+
+// function getRandomInt(max) {
+//     return Math.floor(Math.random() * max);
+// }
+
+// let a = prompt('Введите сюда число лт 0 до 59')
+// if (a >= 0 && a <= 14) {
+//     alert('Первая четверть')
+// }
+// else if (a >= 15 && a <= 29) {
+//     alert('Вторая четверть')
+// }
+// else if (a >= 30 && a <= 44) {
+//     alert('Третья четверть')
+// }
+// else if (a >= 45 && a <= 59) {
+//     alert('Четвертая четверть')
+// }
+// else{
+//     alert('Ошибка')
+// }
